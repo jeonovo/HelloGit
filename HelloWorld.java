a9f9158021fe8deae560b97a505e73d380eddb30
Jonny Bland <jonnyjames.bland@gmail.com>
2015-03-18 16:16:35 +0000

    Changed print line
    Changed print line from hi to yes yes hello

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+	public HelloWorld() {
+	
+		System.out.println("yes yes hello");
+		
+	}
+	
+	public static void main (String args[]) {
+	
+		new HelloWorld();
+		
+	}
+	
+}
